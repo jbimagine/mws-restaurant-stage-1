d5b2e24b40bc06acc2ae6b7c147696e9741ee43a
Joe Bates <joebates@jbimagine.com>
2018-09-04 08:22:17 -0700

    review fixes and applied suggestions

--- sw.js
+++ sw.js
@@ -1,48 +1,47 @@
-const cacheName = 'v2';
-
-
-// Call Install Event
-self.addEventListener('install', (e)=> {
-    console.log(`Service Worker: Installed`);
-})
-
-// Call Activate Event
-self.addEventListener('activate', (e)=> {
-    console.log(`Service Worker: Activated`);
-    // Remove unwanted caches
-    e.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if(cache !== cacheName) {
-                        console.log('Service Worker: Clearing Old Cache');
-                        return caches.delete(cache);
-                    }
-                })
-            )
+self.addEventListener('install', function (event) {
+    // Perform install steps
+  }); var cacheVersion = 'version-1';
+  var cacheNames = [
+    '/',
+    './index.html',
+    './restaurant.html',
+    './css/styles.css',
+    './js/dbhelper.js',
+    './js/main.js',
+    './js/restaurant_info.js',
+    './data/restaurants.json',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg',
+  ];
+  
+  self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(cacheVersion)
+        .then(function (cache) {
+          console.log('Opened cache');
+          return cache.addAll(cacheNames);
         })
-    )
-});
-
-// Call Fetch Event
-self.addEventListener('fetch', e => {
-    console.log('Service Worker: Fetching');
-    e.respondWith(
-        fetch(e.request)
-            .then(res => {
-                // Make clone of response
-                const resClone = res.clone();
-                // Open cache
-                caches
-                .open(cacheName)
-                .then(cache => {
-                    // Add response to cache
-                    cache.put(e.request, resClone);
-                });
-                return res;
-            }).catch(err => { caches.match(e.request).then(res => res)})
-    )
-})
-
-
-
+    );
+  });
+  
+  self.addEventListener('activate',  event => {
+    event.waitUntil(self.clients.claim());
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request, {ignoreSearch:true}).then(response => {
+        return response || fetch(event.request);
+      })
+      .catch(err => console.log(err, event.request))
+    );
+  });
